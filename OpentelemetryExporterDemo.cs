8b0022cec739dd5ff364eae410302e5e9933cabd
UnCool-0x <162315894+UnCool-0x@users.noreply.github.com>
2024-07-02 19:47:48 +0530

    OpentelemetryExporterDemo.cs

---
+++ OpentelemetryExporterDemo.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
+
+namespace Demo
+{
+    internal static class OpentelemetryExporterDemo
+    {
+        internal static void Run()
+        {
+            Console.WriteLine("otlp running");
+            var serviceName = "<signoz_service_name>";
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddSource(serviceName)
+                .SetResourceBuilder(
+                ResourceBuilder.CreateDefault().AddService(serviceName))
+                .AddOtlpExporter(opt =>
+                                 {
+                                     opt.Endpoint = new Uri("<signoz_endpoint>");
+                                     opt.Protocol = OtlpExportProtocol.HttpProtobuf;
+                                     opt.Headers = "signoz-access-token=<signoz_ingestion_key>";
+                                 })
+                .AddConsoleExporter()
+                .Build();
+            for(int i = 0; i<10; i++)
+            {
+                var MyActivitySource = new ActivitySource(serviceName);
+                using var activity = MyActivitySource.StartActivity("SayHello");
+                activity?.SetTag("bar", "Hello World");
+            }
+        }
+    }
+}
